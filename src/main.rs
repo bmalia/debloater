8a22b698d30e86947d7dd4435c4cdd38ea220fc2
bmalia <banksmalia1@proton.me>
2025-01-18 19:46:00 -0700

    Add basic OS detection capability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io;
+
+fn get_distribution() -> Option<String> {
+    println!("Detecting linux distribution...");
+    let file = File::open("/etc/os-release").ok()?; // opens /etc/release
+    let reader = BufReader::new(file);
+    
+    for line in reader.lines() { // Searches each line of the file for the 'ID=' tag
+        if let Ok(line) = line {
+            if line.starts_with("ID=") {
+                return Some(line.replace("ID=", "").replace("\"", ""));
+            }
+        }
+    }
+    None
+}
+
+fn handle_debian() {
+    println!("Handling Debian-based distribution");
+    // Add debian-specific logic here
+}
+
+fn handle_arch() {
+    println!("Handling Arch-based distribution");
+    // Add arch-specific logic here
+}
+
+fn handle_fedora() {
+    println!("Handling Fedora-based distribution");
+    // Add fedora-specific logic here
+}
+
 fn main() {
-    println!("Hello, world!");
+    let distro = get_distribution().unwrap_or_else(|| String::from("unknown")); // Grabs the distribution ID
+    
+    match distro.as_str() {
+        "debian" | "ubuntu" | "linuxmint" | "pop" => handle_debian(), // Calls debian function for these distros
+        "arch" | "manjaro" | "endeavouros" => handle_arch(), // Calls arch function for these distros
+        "fedora" => handle_fedora(), // Calls fedora function for this distro TODO: do research and potentially add more distros here (eg. nobara)
+        _ => {
+            println!("Unsupported distribution: {}", distro);
+            println!("If you know what your distribution is based on, enter it now:");
+            let mut distro = String::new();
+            io::stdin().read_line(&mut distro).expect("Failed to read line");
+            match distro.trim() {
+                "debian" | "ubuntu" | "linuxmint" | "pop" => handle_debian(), // Calls debian function for these distros
+                "arch" | "manjaro" | "endeavouros" => handle_arch(), // Calls arch function for these distros
+                "fedora" => handle_fedora(), // Calls fedora function for this distro TODO: do research and potentially add more distros here (eg. nobara)
+                _ => println!("Unsupported distribution: {}", distro),
+            }
+        }
+    }
 }
